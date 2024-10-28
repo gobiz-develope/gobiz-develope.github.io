b8c16432862fb80d8e40ff7f0e465d802034b077
KostsPlayer <KostsPlayer@users.noreply.github.com>
2024-10-28 09:05:46 +0000

    deploy: 7bbbfb8254794c8a236359ce36da8222f74f25c9

--- src/pages/home/partners/slide.js
+++
@@ -1,9 +0,0 @@
-export default function slide() {
-  const logoSlide = document.querySelector(".logo-slide");
-  if (logoSlide) {
-    const clone = logoSlide.cloneNode(true);
-    logoSlide.parentNode.appendChild(clone);
-  } else {
-    console.error("Element '.logo-slide' not found.");
-  }
-}
